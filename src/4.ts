d01706fc1d829494a7b88f704383862c108953fb
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-06-30 12:20:24 +0300

    homework implementation

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,39 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+class MyHouse {
+  private tenants: Person[];
+
+  constructor(private key: Key) {}
+
+  openDoor(key: Key): boolean {
+    return this.key.getSignature() === key.getSignature();
+  }
+
+  comeIn(person: Person) {
+    if (this.openDoor(person.getKey())) {
+      this.tenants.push(person);
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +42,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
